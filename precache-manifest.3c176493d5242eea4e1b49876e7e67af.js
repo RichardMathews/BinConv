a74affc16ae6bd7c73060940ff1fd5f40b267c16
RichardMathew <richardmathewsa.silva@hotmail.com>
2020-04-06 14:50:04 -0300

    Updates

---
+++ precache-manifest.3c176493d5242eea4e1b49876e7e67af.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee5eaa4aaf10e9deaee5da16d9bfc063",
+    "url": "/binconv/index.html"
+  },
+  {
+    "revision": "51074ae4132b7deb3623",
+    "url": "/binconv/static/js/2.853f6bf2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/binconv/static/js/2.853f6bf2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfd5f3105c653a9af2c3",
+    "url": "/binconv/static/js/main.906ea317.chunk.js"
+  },
+  {
+    "revision": "679f77fd31f68ef1e227",
+    "url": "/binconv/static/js/runtime-main.0407d42b.js"
+  }
+]);
